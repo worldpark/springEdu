f715fe030275b23bd9b069c68f645e9e6cc62ea0
worldpark <vbwjsdl@naver.com>
2024-04-27 18:10:20 +0900

    230427 스프링 git 재등록

---
+++ src/main/java/com/example/shop/CodingAppleSpringApplication.java
@@ -0,0 +1,13 @@
+package com.example.shop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodingAppleSpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CodingAppleSpringApplication.class, args);
+    }
+
+}
